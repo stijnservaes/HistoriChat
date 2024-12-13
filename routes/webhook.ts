a184de56104f560e8a006fcbfcf2ddd9ef1caac3
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-12-13 13:29:30 +0100

    feat: Modify Username Webhook

--- routes/webhook.ts
+++ routes/webhook.ts
@@ -14,23 +14,113 @@ type Evt = {
   };
 };
 
-router.all("/", async (req: Request, res: Response, next: NextFunction) => {
-  if (!process.env.SIGNING_SECRET) {
-    throw new Error("Set SIGNING_SECRET in .env");
-  }
+router.all(
+  "/create",
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (!process.env.SIGNING_SECRET_CREATE) {
+      throw new Error("Set SIGNING_SECRET in .env");
+    }
 
-  const wh = new Webhook(process.env.SIGNING_SECRET);
+    const evt = setupWebhook(req, res, process.env.SIGNING_SECRET_CREATE);
+
+    if (typeof evt === "string") {
+      return void res.status(400).json({ success: false, message: evt });
+    }
+
+    try {
+      await User.create({
+        clerkId: evt.data.id,
+        username: evt.data.username,
+      });
+    } catch (error: any) {
+      if (error.code === 11000) {
+        return void res.json({
+          success: true,
+          message: "Username already exists. Database not modified",
+        });
+      } else if (error instanceof Error) {
+        console.error(
+          "Error: Could not add created Clerk user to MongoDB: ",
+          error.message
+        );
+        return void res.status(400).json({
+          success: false,
+          message: error.message,
+        });
+      } else {
+        console.error(
+          "Unknown error has occurred when adding Clerk user to MongoDB"
+        );
+        return void res.status(400).json({
+          success: false,
+          message:
+            "Unknown error has occurred when adding Clerk user to MongoDB",
+        });
+      }
+    }
+
+    res.json({
+      success: true,
+      message: "Created user added to MongoDB",
+    });
+  }
+);
+
+router.all(
+  "/modify",
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (!process.env.SIGNING_SECRET_MODIFY) {
+      throw new Error("Set SIGNING_SECRET in .env");
+    }
+
+    const evt = setupWebhook(req, res, process.env.SIGNING_SECRET_MODIFY);
+
+    if (typeof evt === "string") {
+      return void res.status(400).json({ success: false, message: evt });
+    }
+
+    try {
+      await User.updateOne(
+        { clerkId: evt.data.id },
+        { username: evt.data.username }
+      );
+    } catch (error: any) {
+      if (error instanceof Error) {
+        console.error(
+          "Error: could not modify username to MongoDB: ",
+          error.message
+        );
+        return void res
+          .status(400)
+          .json({ success: false, message: error.message });
+      } else {
+        console.error(
+          "Unknown error has occurred when modifying Clerk username to MongoDB"
+        );
+        return void res.status(400).json({
+          success: false,
+          message:
+            "Unknown error has occurred when modifying Clerk username to MongoDB",
+        });
+      }
+    }
+
+    res.json({
+      success: true,
+      message: "Modified username added to MongoDB",
+    });
+  }
+);
+
+function setupWebhook(req: Request, res: Response, secret: string) {
+  const wh = new Webhook(secret);
 
   const svix_signature = req.headers["svix-signature"];
   const svix_id = req.headers["svix-id"];
   const svix_timestamp = req.headers["svix-timestamp"];
 
   if (!svix_id || !svix_signature || !svix_timestamp) {
-    res.status(400).json({
-      success: false,
-      message: "Error: Missing svix headers",
-    });
-    return;
+    return "Error: Missing svix headers";
   }
 
   let evt: Evt;
@@ -44,54 +134,14 @@ router.all("/", async (req: Request, res: Response, next: NextFunction) => {
   } catch (error) {
     if (error instanceof Error) {
       console.error("Error: Could not verify webhook:", error.message);
-      return void res.status(400).json({
-        success: false,
-        message: error.message,
-      });
+      return error.message;
     } else {
       console.error("Unknown error has occurred when verifying webhook.");
-      return void res.status(400).json({
-        success: false,
-        message: "Unknown error has occurred when verifying webhook.",
-      });
+      return "Unknown error has occurred when verifying webhook.";
     }
   }
 
-  try {
-    await User.create({
-      clerkId: evt.data.id,
-      username: evt.data.username,
-    });
-  } catch (error: any) {
-    if (error.code === 11000) {
-      return void res.json({
-        success: true,
-        message: "Username already exists. Database not modified",
-      });
-    } else if (error instanceof Error) {
-      console.error(
-        "Error: Could not add created Clerk user to MongoDB: ",
-        error.message
-      );
-      return void res.status(400).json({
-        success: false,
-        message: error.message,
-      });
-    } else {
-      console.error(
-        "Unknown error has occurred when adding Clerk user to MongoDB"
-      );
-      return void res.status(400).json({
-        success: false,
-        message: "Unknown error has occurred when adding Clerk user to MongoDB",
-      });
-    }
-  }
-
-  res.json({
-    success: true,
-    message: "Created user added to MongoDB",
-  });
-});
+  return evt;
+}
 
 export default router;
